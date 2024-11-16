8421935c21560224f5de6575d82762af741a9466
tyk326 <123436909+tyk326@users.noreply.github.com>
2024-11-16 17:39:23 -0500

    Delete Snake2 directory

--- Snake2/Main.java
+++
@@ -1,6 +0,0 @@
-public class Main{
-    public static void main(String[] args){
-        Frame x=new Frame();
-        x.display();
-    }
-}
--- Snake2/Tile.java
+++
@@ -1,8 +0,0 @@
-public class Tile {
-    private int x, y;
-
-    public Tile(int x, int y){
-        this.x=x;
-        this.y=y;
-    }
-}
