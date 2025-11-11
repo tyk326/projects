b9ea3bb9437312ea999647a4db749874c8c01792
Tyler <tyk326@lehigh.edu>
2025-11-10 22:03:07 -0500

    some stuff

--- Destined/backend/main.py
+++ Destined/backend/main.py
@@ -1,20 +1,38 @@
 #to run with flask: flask --app main run
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
-import csv
+from supabase import create_client, Client
+from dotenv import load_dotenv
+import os
+
+# Load environment variables from .env
+load_dotenv()
 
 app = Flask(__name__) #__name__ is like 'this'. Creates an instance
 CORS(app) #to serve frontend. https://medium.com/@mterrano1/cors-in-a-flask-api-38051388f8cc
 
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-# @app.route('/')
-# def home():
-#     return "Welcome to this page!"
+@app.route('/')
+def home():
+    return "Welcome to this page!"
 
+@app.route("/login", methods=["POST"])
+def login():
+    data = request.json()
+    email = data.get("email")
+    password = data.get("password")
+    result = supabase.auth.sign_in_with_password({"email": email, "password": password})
+    if (result.user):
+        return jsonify({"message": "Login Successful", "access_token": result.session.access_token})
+    else:
+        return jsonify({"error": "Invalid credentials"}), 401
 
-@app.route('/network') #tells what url triggers the function
-def networkInfo():
-    info = []
-    for movie in movies:
-        info.append({"title": movie.title, "rank": movie.rank, "actors": movie.actors})
-    return jsonify(info)
+if __name__ == "__main__":
+    app.run(debug=True) 
+
+# debug=True
+# Automatic reload: The server restarts when you change the code.
+# Debug mode: Shows detailed error pages in your browser with stack traces.
